e24eac1c0ff673a04669e16c058533b0da94d64f
Richard-Drogo <richard.drogo@gmail.com>
2021-01-14 11:27:55 +0100

    Updated QueryParsingInsertTests and added some Error Message Constants for BadQueryException

--- src/test/java/fr/tse/db/query/QueryParsingDeleteTests.java
+++ src/test/java/fr/tse/db/query/QueryParsingDeleteTests.java
@@ -2,16 +2,21 @@ package fr.tse.db.query;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import fr.tse.db.query.error.BadQueryException;
 import fr.tse.db.query.service.QueryService;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 class QueryParsingDeleteTests {
 
 	@Autowired
 	private QueryService qs;
-	private final static String ACTION = "DELETE";
+	private final static String ACTION = "delete";
 	
 	// ---------------------- [DELETE ALL] [SINGLEQUERY] [BadQueryException]
 	@Test
