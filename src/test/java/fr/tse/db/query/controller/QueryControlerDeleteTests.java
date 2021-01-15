b70d3a10630160d5ad768d8755020a9470d5f91f
ArnaudT13 <55779980+ArnaudT13@users.noreply.github.com>
2021-01-15 14:11:16 +0100

    Init query create file test

---
+++ src/test/java/fr/tse/db/query/controller/QueryControlerDeleteTests.java
@@ -0,0 +1,14 @@
+package fr.tse.db.query.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QueryControlerDeleteTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
