5d4303f2de02e83009148779bf5f2fec9418770e
PierreTSE <pierre--giraud@orange.fr>
2021-01-15 16:06:21 +0100

    gestion des cors
    Signed-off-by: PierreTSE <pierre--giraud@orange.fr>

--- src/main/java/fr/tse/db/query/controller/QueryController.java
+++ src/main/java/fr/tse/db/query/controller/QueryController.java
@@ -2,23 +2,25 @@ package fr.tse.db.query.controller;
 
 import java.util.HashMap;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import fr.tse.db.query.service.QueryService;
 
+@Slf4j
 @RestController
 public class QueryController {
 
     @Autowired
     QueryService queryService;
 
-    @PostMapping(value = "/query",produces = "application/json")
+    @CrossOrigin(origins = "http://localhost:8080")
+    @PostMapping(value = "/query", produces = "application/json")
     public ResponseEntity<?> postQuery(@RequestParam("query") String query) throws Exception {
+       log.debug(query);
         // Get object
         HashMap<String, Object> response = this.queryService.handleQuery(query);
         HashMap<String, Object> responseGlobal = new HashMap<>();
